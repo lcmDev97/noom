e84ce4d8df317011d9d33b5f82cfea3e7088c481
lcmDev97 <lcmdev97@gmail.com>
2023-05-04 16:18:37 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,77 @@
+import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    });
+
+});
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
+
+
+// ------------------------------아래는 내 코드
+// import http from "http"
+// import { Server } from "socket.io";
+// import express from "express"
+//
+// const app = express()
+// const PORT = 3000
+//
+// app.set("view engine", "pug");
+// app.set("views", __dirname + "/views");
+// app.use("/public", express.static(__dirname + "/public"));
+//
+// app.get( "/", (req, res)=>{
+//     res.render("home")
+// })
+//
+// app.get("/*", (req, res)=>{
+//     res.redirect("/")
+// })
+//
+// const httpServer = http.createServer(app)
+// const wsServer = new Server(httpServer);
+//
+// wsServer.on("connection", (socket) => {
+//
+//     socket.on("join_room", (roomName, ) => {
+//         socket.join(roomName)
+//         socket.to(roomName).emit("welcome")
+//     })
+//
+//     socket.on("offer", (offer, roomName) => {
+//         socket.to(roomName).emit("offer", offer)
+//     })
+//
+//
+// })
+//
+// httpServer.listen(PORT, ()=>{
+//     console.log(`Running on Port: ${PORT}`)
+// })
